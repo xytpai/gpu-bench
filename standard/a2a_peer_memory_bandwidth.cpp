f3abe1ab172d19fd16e61d2c8be210fa672c8de5
xytpai <xytpai@foxmail.com>
2025-10-11 06:12:12 +0000

    refine a2a code 4

--- standard/a2a_peer_memory_bandwidth.cpp
+++ standard/a2a_peer_memory_bandwidth.cpp
@@ -32,7 +32,7 @@ std::vector<Path> enable_p2p(int *ngpus) {
 struct GPUResources {
     int buffer_bytes;
     std::vector<void *> send_buffers;
-    std::vector<void *> recv_buffers;
+    std::vector<std::vector<void *>> recv_buffers;
     std::vector<gpuStream_t> streams;
 };
 
@@ -40,15 +40,16 @@ void run_a2a(std::vector<Path> &paths, std::vector<GPUResources> &rs) {
     for (auto &p : paths) {
         int local = p.local;
         int peer = p.peer;
-        gpuSetDevice(peer);
-        int nbuffers = rs[peer].recv_buffers.size();
-        int nstreams = rs[peer].streams.size();
-        int buffer_bytes = rs[peer].buffer_bytes;
         // local -> peer
+        gpuSetDevice(peer);
+        int nbuffers = rs[peer].send_buffers.size();
+        int buffer_bytes = rs[peer].buffer_bytes;
+        int nstreams = rs[peer].streams.size();
         for (int b = 0; b < nbuffers; ++b) {
             int s_peer = b % nstreams;
-            gpuMemcpyPeerAsync(rs[peer].recv_buffers[b], peer, rs[local].send_buffers[b],
-                               local, buffer_bytes, rs[peer].streams[s_peer]);
+            gpuMemcpyPeerAsync(rs[peer].recv_buffers[local][b], peer,
+                               rs[local].send_buffers[b], local,
+                               buffer_bytes, rs[peer].streams[s_peer]);
         }
     }
     for (int g = 0; g < rs.size(); ++g) {
@@ -57,19 +58,23 @@ void run_a2a(std::vector<Path> &paths, std::vector<GPUResources> &rs) {
     }
 }
 
-void validate(std::vector<GPUResources> &rs, unsigned char val) {
+void validate(std::vector<GPUResources> &rs, unsigned char flag) {
     for (int g = 0; g < rs.size(); ++g) {
         gpuSetDevice(g);
-        int bb = rs[g].buffer_bytes;
-        for (auto &ptr_ : rs[g].recv_buffers) {
-            auto ptr = (unsigned char *)ptr_;
-            bool c0 = val == *ptr;
-            bool c1 = (val + 1) == *(ptr + bb - 1);
-            if (c0 && c1) {
-                // do nothing;
-            } else {
-                std::cout << "val:error!\n";
-                return;
+        int buffer_bytes = rs[g].buffer_bytes;
+        for (int rg = 0; rg < rs[g].recv_buffers.size(); ++rg) {
+            if (g == rg) continue;
+            for (int b = 0; b < rs[g].recv_buffers[rg].size(); ++b) {
+                auto ptr = (unsigned char *)rs[g].recv_buffers[rg][b];
+                auto flag_ = flag + rg * 10 + b;
+                bool c0 = (flag_) == *ptr;
+                bool c1 = (flag_ + 1) == *(ptr + buffer_bytes - 1);
+                if (c0 && c1) {
+                    // do nothing;
+                } else {
+                    std::cout << "val:error!\n";
+                    return;
+                }
             }
         }
     }
@@ -77,22 +82,31 @@ void validate(std::vector<GPUResources> &rs, unsigned char val) {
 
 void measure_peer_bandwidth(size_t buffer_bytes, int num_buffers, int streams_per_gpu, int iters, int warmups = 2) {
     int ngpus = 0;
+    std::cout << "enable p2p ... \n";
     auto paths = enable_p2p(&ngpus);
 
     // allocate resources
+    std::cout << "allocate resources ... \n";
     std::vector<GPUResources> rs(ngpus);
+
+    unsigned char flag = 0x05;
     for (int g = 0; g < ngpus; ++g) {
         gpuSetDevice(g);
         rs[g].send_buffers.resize(num_buffers);
-        rs[g].recv_buffers.resize(num_buffers);
         rs[g].buffer_bytes = buffer_bytes;
         for (int b = 0; b < num_buffers; ++b) {
             gpuMalloc(&rs[g].send_buffers[b], buffer_bytes);
-            gpuMalloc(&rs[g].recv_buffers[b], buffer_bytes);
             // Initialize memory with something to avoid zero-page optimizations
-            gpuMemset(rs[g].send_buffers[b], 0xA5, 1);
-            gpuMemset((unsigned char *)rs[g].send_buffers[b] + buffer_bytes - 1, 0xA6, 1);
-            gpuMemset(rs[g].recv_buffers[b], 0x01, buffer_bytes);
+            auto flag_ = flag + g * 10 + b;
+            gpuMemset(rs[g].send_buffers[b], flag_, 1);
+            gpuMemset((unsigned char *)rs[g].send_buffers[b] + buffer_bytes - 1, flag_ + 1, 1);
+        }
+        rs[g].recv_buffers.resize(ngpus);
+        for (int rg = 0; rg < ngpus; ++rg) {
+            rs[g].recv_buffers[rg].resize(num_buffers);
+            for (int b = 0; b < num_buffers; ++b) {
+                gpuMalloc(&rs[g].recv_buffers[rg][b], buffer_bytes);
+            }
         }
         rs[g].streams.resize(streams_per_gpu);
         for (int s = 0; s < streams_per_gpu; ++s) {
@@ -101,21 +115,24 @@ void measure_peer_bandwidth(size_t buffer_bytes, int num_buffers, int streams_pe
     }
 
     // warmup
+    std::cout << "warmup ... \n";
     for (int w = 0; w < warmups; ++w) {
         run_a2a(paths, rs);
-        validate(rs, 0xA5);
+        validate(rs, flag);
     }
 
+    flag += 1;
     for (int g = 0; g < ngpus; ++g) {
         gpuSetDevice(g);
         for (int b = 0; b < num_buffers; ++b) {
-            gpuMemset(rs[g].send_buffers[b], 0xA3, 1);
-            gpuMemset((unsigned char *)rs[g].send_buffers[b] + buffer_bytes - 1, 0xA4, 1);
-            gpuMemset(rs[g].recv_buffers[b], 0x02, buffer_bytes);
+            auto flag_ = flag + g * 10 + b;
+            gpuMemset(rs[g].send_buffers[b], flag_, 1);
+            gpuMemset((unsigned char *)rs[g].send_buffers[b] + buffer_bytes - 1, flag_ + 1, 1);
         }
     }
 
     // run
+    std::cout << "run iters ... \n";
     auto t0 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < iters; ++i) {
         run_a2a(paths, rs);
@@ -133,19 +150,21 @@ void measure_peer_bandwidth(size_t buffer_bytes, int num_buffers, int streams_pe
     std::cout << "streams per gpu: " << streams_per_gpu << "\n";
     std::cout << "bandwidth total: " << bw_total << " GBps\n";
 
-    validate(rs, 0xA3);
+    validate(rs, flag);
 
     // cleanup
     for (int g = 0; g < ngpus; ++g) {
         gpuSetDevice(g);
         for (auto s : rs[g].streams) gpuStreamDestroy(s);
         for (auto b : rs[g].send_buffers) gpuFree(b);
-        for (auto b : rs[g].recv_buffers) gpuFree(b);
+        for (auto &buf : rs[g].recv_buffers) {
+            for (auto b : buf) gpuFree(b);
+        }
     }
 }
 
 int main() {
-    std::cout << "a2a copy test ... \n";
+    std::cout << "======== A2A COPY TEST ========\n";
     size_t buffer_bytes = (size_t)1024 * 1024 * 1024 / 8;
     int num_buffers = 64;
     int streams_per_gpu = 64;
