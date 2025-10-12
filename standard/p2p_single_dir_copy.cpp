3011828d5c9d675328444d5a6deb960f1074a18d
xytpai <xytpai@foxmail.com>
2025-10-12 03:06:20 +0000

    update single dir cpy

--- standard/p2p_single_dir_copy.cpp
+++ standard/p2p_single_dir_copy.cpp
@@ -4,35 +4,39 @@
 #include "utils.h"
 using namespace std;
 
+void sync_devices(int dst, int src) {
+    gpuSetDevice(src);
+    gpuDeviceSynchronize();
+    gpuSetDevice(dst);
+    gpuDeviceSynchronize();
+}
+
 float measure_peer_bandwidth(int dst_dev, int src_dev, size_t bytes) {
     gpuSetDevice(src_dev);
     void *d_src = nullptr;
     gpuMalloc(&d_src, bytes);
     gpuMemset(d_src, 0x7f, bytes);
+    gpuDeviceSynchronize();
 
     gpuSetDevice(dst_dev);
     void *d_dst = nullptr;
     gpuMalloc(&d_dst, bytes);
     gpuMemset(d_dst, 0x00, bytes);
+    gpuDeviceSynchronize();
 
-    gpuSetDevice(src_dev);
-    gpuStream_t s;
-    gpuStreamCreate(&s);
-
-    gpuSetDevice(dst_dev);
     gpuEvent_t start, stop;
     gpuEventCreate(&start);
     gpuEventCreate(&stop);
 
     for (int wm = 0; wm < 2; ++wm) {
-        gpuMemcpyPeerAsync(d_dst, dst_dev, d_src, src_dev, bytes, s);
-        gpuStreamSynchronize(s);
+        gpuMemcpyPeerAsync(d_dst, dst_dev, d_src, src_dev, bytes);
+        sync_devices(dst_dev, src_dev);
     }
 
     gpuSetDevice(dst_dev);
     gpuEventRecord(start);
-    gpuMemcpyPeerAsync(d_dst, dst_dev, d_src, src_dev, bytes, s);
-    gpuStreamSynchronize(s);
+    gpuMemcpyPeerAsync(d_dst, dst_dev, d_src, src_dev, bytes);
+    sync_devices(dst_dev, src_dev);
     gpuEventRecord(stop);
     gpuEventSynchronize(stop);
     float ms = 0;
@@ -40,7 +44,6 @@ float measure_peer_bandwidth(int dst_dev, int src_dev, size_t bytes) {
 
     gpuEventDestroy(start);
     gpuEventDestroy(stop);
-    gpuStreamDestroy(s);
     gpuFree(d_src);
     gpuFree(d_dst);
 
@@ -51,7 +54,7 @@ float measure_peer_bandwidth(int dst_dev, int src_dev, size_t bytes) {
 }
 
 int main() {
-    std::cout << "1GB peer single copy test ... (GBps)\n";
+    std::cout << "1GB peer single dir copy test ... (GBps)\n";
     enable_p2p();
     int device_count = 0;
     gpuGetDeviceCount(&device_count);
@@ -63,7 +66,7 @@ int main() {
     for (int dst = 0; dst < device_count; ++dst) {
         std::cout << std::right << std::setw(11) << ("[" + std::to_string(dst) + "]");
         for (int src = 0; src < device_count; ++src) {
-            auto bw = measure_peer_bandwidth(dst, src, (size_t)2 * 1024 * 1024 * 1024);
+            auto bw = measure_peer_bandwidth(dst, src, (size_t)1 * 1024 * 1024 * 1024);
             std::cout << std::setw(10) << std::fixed << std::setprecision(3) << bw << " ";
         }
         std::cout << "\n";
